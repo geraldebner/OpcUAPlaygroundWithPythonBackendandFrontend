49bbbb3a1b85e9baf2a9d651295ec5e52350a4f7
Gerald Ebner <gebner@lr.htlweiz.at>
2025-10-14 20:12:18 +0200

    add test client

---
+++ opcua_simple_console_client/TestClient.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Threading.Tasks.Dataflow;
+using Opc.UaFx.Client;
+
+namespace opcua_simple_console_client;
+
+/// <summary>
+/// This sample demonstrates how to implement a primitive OPC UA client.
+/// </summary>
+public class TestClient
+{
+    
+    
+    OpcClient client = new OpcClient("opc.tcp://localhost:4840");
+
+    public void RunTest()
+    {
+        client.Connect();
+        Read();
+        Write();
+        Subscribe();
+        client.Disconnect();
+
+
+    }
+
+    private void Read()
+    {
+        Console.WriteLine("Test READ:");
+        Console.WriteLine("ReadNode: {0}", client.ReadNode("ns=2;s=Device1.SimValue1"));
+    }
+
+    private void Write()
+    {
+        Console.WriteLine("Test WRITE:");
+        Console.WriteLine("ReadNode: {0}", client.ReadNode("ns=2;s=Device1.ParamValue1"));
+        client.WriteNode("ns=2;s=Device1.ParamValue1", false);
+        Console.WriteLine("ReadNode: {0}", client.ReadNode("ns=2;s=Device1.ParamValue1"));
+    }
+
+    
+
+    private void Subscribe()
+    {
+        Console.WriteLine("Test SUBSCRIBE:");
+        client.SubscribeNodes(
+            new OpcSubscribeDataChange("ns=2;s=Device1.SimValue1", HandleEventReceived),
+            new OpcSubscribeDataChange("ns=2;s=Device1.SimValue2", HandleEventReceived));
+
+        Console.WriteLine("ReadNode: {0}", client.ReadNode("ns=2;s=Device1.SimValue1"));
+    
+        for (int i = 0; i < 30; i++)
+        {
+            client.WriteNode("ns=2;s=Device1.ParamValue1", i);
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+
+     public  void HandleEventReceived(object sender, OpcDataChangeReceivedEventArgs e)
+    {
+        Console.WriteLine("DataChange: {0} = {1}", e.MonitoredItem.NodeId, e.Item.Value);
+    }
+}
