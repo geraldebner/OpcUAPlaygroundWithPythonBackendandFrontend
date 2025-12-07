b2f4da5a0df5d90751c4aa7cce6e3382c0dcd598
Gerald Ebner <gebner@lr.htlweiz.at>
2025-12-07 13:15:19 +0100

    make application

---
+++ VentiltesterFrontend/START_FRONTEND.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Starting VentilTester Frontend...
+echo.
+echo The frontend will be available at: http://localhost:3000
+echo Press Ctrl+C to stop the server
+echo.
+node server.js
+pause
